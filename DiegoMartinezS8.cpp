4f827279cbdbf88c484f9968cc90295b19c3161a
Diego Martinez <df.martinezv@uniandes.edu.co>
2018-09-26 13:13:22 -0500

    Add files via upload

---
+++ DiegoMartinezS8.cpp
@@ -0,0 +1,18 @@
+// Ejercicio semana 8 
+#include<iostream>
+using namespace std;
+int main(){ 
+for(int i =1; i<=10;i++){
+
+	cout<<"Hello world     "<<i<<"\n";
+	}
+int contador = 1;
+while(contador<=10){
+	cout<<"Hello World    "<<contador<<"\n";
+	contador ++; 
+}
+return 0;
+}
+
+
+	
